cc4327bac90f369e2645b46b479a27918b5acebb
steven victor <chikodi543@gmail.com>
2019-06-06 14:43:26 +0100

    Define a struct inside another one

--- main.go
+++ main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type conntactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   conntactInfo
 }
 
 func main() {
@@ -12,9 +18,20 @@ func main() {
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
 
 	//declare a variable of type person, third way to declare struct
-	var alex person
-	alex.firstName = "steven"
-	alex.lastName = "Oke"
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// var alex person
+	// alex.firstName = "steven"
+	// alex.lastName = "Oke"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Ade",
+		contact: conntactInfo{
+			email:   "jim@jim.com",
+			zipCode: 17882,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
