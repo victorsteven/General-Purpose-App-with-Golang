54bc24d18547af348d7bfdfe243134558e9470c9
steven victor <chikodi543@gmail.com>
2019-06-06 14:34:24 +0100

    third way to declare struct

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ type person struct {
 
 func main() {
 	// alex := person{"Alex", "Anderson"}
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
 
+	//declare a variable of type person, third way to declare struct
+	var alex person
+	alex.firstName = "steven"
+	alex.lastName = "Oke"
 	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
 }
